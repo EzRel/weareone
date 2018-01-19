a8e7c4c2b51b85758035e938fffc506dcf67a2a4
EzRel <35404657+EzRel@users.noreply.github.com>
2018-01-19 16:15:49 +0200

    Update run.py

--- run.py
+++ run.py
@@ -113,7 +113,8 @@ async def website():
 async def cumpara(ctx, pid = ''):
 	'''Dupa ce ti-ai achizitionat produsul, foloseste comanda ca sa primesti tot.'''
 	if pid != "":
-		pinfo = urllib.request.urlopen("http://waodiscord.000webhostapp.com/purchases/%s.txt"%pid).read(1000).split("`")
+		pinfo = urllib.request.urlopen("http://waodiscord.000webhostapp.com/purchases/%s.txt"%pid).read(1000)
+		pinfo = pinfo.split("`")
 		itemspr = "- Produse: %s"%int(pinfo[1])
 		for x in range(int(pinfo[1])):
 			curr = pinfo[x + 1].split("~")
