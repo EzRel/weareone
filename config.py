ff3eed1119c2c2b8be72794d21d145eb974ea1d9
EzRel <35404657+EzRel@users.noreply.github.com>
2018-03-14 18:02:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 
-prefix = 'w.' #put prefix here
+prefix = 'cloudr, ' #put prefix here
 
 link = 'https://discordapp.com/oauth2/authorize?client_id=399949104068952064&scope=bot&permissions=2152' #put bot invite link here
 
